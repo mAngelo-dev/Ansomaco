4aeab3a2fcafc68a3a6828b77419f162b57bff22
mAngelo-dev <m.angelodev@gmail.com>
2024-11-05 16:08:54 -0300

    Adiciona mensagem de Bom Dia,Tarde ou Noite

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,16 +1,17 @@
 export default function Dashboard() {
 
-    function getSaudation() {
-        const hour = new Date().getHours();
+  function getSaudation() {
+    const hour = new Date().getHours();
 
-        if (hour >= 0 && hour < 12) {
-            return 'Bom dia';
-        }  else if (hour >= 12 && hour < 18) {
-            return 'Boa tarde';
-        } else {
-            return 'Boa noite';
-        }
-    };
+    if (hour >= 0 && hour < 12) {
+      return 'Bom dia';
+    }  else if (hour >= 12 && hour < 18) {
+      return 'Boa tarde';
+    } else {
+      return 'Boa noite';
+    }
+  }
+
   return (
     <>
       <h1>{getSaudation()}</h1>
